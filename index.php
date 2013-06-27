0370fd4e238015dfd2fa474e393c0ef34c5e46ea
leonmagee <leonmagee@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-27 22:08:13 +0000

    update to 1.0.6
    git-svn-id: https://plugins.svn.wordpress.org/easy-font-icons/trunk@733120 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: Easy Font Icons
 Plugin URI: http://mageemedia.net
 Description: Choose from 100s of font icons to add to your posts, pages, or custom post types.
-Version: 1.0.5
+Version: 1.0.6
 Author: Leon Magee
 Author URI: http://mageemedia.net
 License: GPL3
