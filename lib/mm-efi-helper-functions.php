c8f28f010e24ba0c1c8a17c634ebcef66df52a54
leonmagee <leonmagee@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-30 04:47:47 +0000

    update to 1.0.10
    git-svn-id: https://plugins.svn.wordpress.org/easy-font-icons/tags/1.0.10@734218 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/mm-efi-helper-functions.php
+++ lib/mm-efi-helper-functions.php
@@ -8,38 +8,6 @@
 
 class mm_efi_helper_functions {
 
-
-    public static function style_tester() {
-
-        for ( $x = 1; $x < 7; ++$x ) {
-
-            echo "<h" . $x . ">Heading Demo H" . $x . "</h" . $x . ">";
-
-        }
-
-        echo "<a href='#' class='button-primary'>Button Primary</a><br /><br />";
-
-        echo "<a href='#' class='button'>Button</a><br /><br />";
-
-        echo "<a href='#' class='button-secondary'>Button Secondary</a><br /><br />";
-
-        echo "<a href='#'>Link Regular</a><br /><br />";
-
-        echo "<div class='error'>Class Error</div><br /><br />";
-
-        echo "<div class='updated'>Class Updated</div><br /><br />";
-
-        echo "<div class='tablenav'><div class='tablenav-pages'>
-
-        <a href='#' class='page-numbers current'>1</a>
-
-        <a href='#' class='page-numbers'>2</a>
-
-        <a href='#' class='page-numbers'>3</a>
-
-        <a href='#' class='page-numbers next'>&raquo</a>
-
-        </div></div>";
-    }
-
+    
+    
 }
